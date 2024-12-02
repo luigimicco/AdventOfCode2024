707e8b73bafcd38836a7f5db751e38cb2a380047
Luigi Micco <luigi.micco@gmail.com>
2024-12-02 09:37:16 +0100

    refactoring

--- Day 02/step1.dart
+++ Day 02/step1.dart
@@ -8,27 +8,35 @@ void main() {
   rows.forEach((row) {
     List<String> levels = row.split(" ");
 
-    bool safe = true;
-    int dir = 0;
-    int prev = int.parse(levels[0]);
-    dir = (int.parse(levels[1]) - prev).sign;
-    for (var i = 1; i < levels.length; i++) {
-      int diff = int.parse(levels[i]) - prev;
-
-      if (!((diff > 0 && diff < 4 && dir == 1) ||
-          (diff < 0 && diff > -4 && dir == -1))) {
-        safe = false;
-      }
-      prev = int.parse(levels[i]);
+    List<int> report = [];
+    for (var i = 0; i < levels.length; i++) {
+      report.add(int.parse(levels[i]));
     }
-    if (safe) safeCount++;
+    if (isSafe(report)) safeCount++;
   });
 
   print(safeCount);
 }
 
+bool isSafe(List<int> report) {
+  bool safe = true;
+  int dir = 0;
+  int prev = report[0];
+  dir = (report[1] - prev).sign;
+  for (var i = 1; i < report.length && safe; i++) {
+    int diff = (report[i] - prev) * dir;
+
+    if (diff <= 0 || diff > 3) {
+      safe = false;
+    }
+    prev = report[i];
+  }
+
+  return safe;
+}
+
 List<String> getRows() {
-  File file = File("data.txt");
+  File file = File("test.txt");
   var fileContent = file.readAsStringSync();
   return fileContent.split("\n");
 }
