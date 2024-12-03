d93c02c39bce11c8881d9dac704f75e780e78463
Luigi Micco <luigi.micco@gmail.com>
2024-12-03 12:42:09 +0100

    [feat] step 1 done

---
+++ Day 03/step1.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+void main() {
+  String row = getRow();
+
+  RegExp exp = RegExp(r'(?:mul\((\d{1,3})),(\d{1,3})\)');
+  String str = row; // 'Parse my string';
+  Iterable<RegExpMatch> matches = exp.allMatches(str);
+
+  int score = 0;
+  for (final m in matches) {
+    score = score +
+        int.parse(m[1].toString().toString()) *
+            int.parse(m[2].toString().toString());
+  }
+  print(score);
+}
+
+String getRow() {
+  File file = File("data.txt");
+  var fileContent = file.readAsStringSync();
+  return fileContent;
+}
